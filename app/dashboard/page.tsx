d84c0203b4c1e32f57e5ad046102a53b8178b85f
smnshzh <smnshzh@gmail.com>
2024-03-25 01:39:53 +0330

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Login from './ui/loginfrom'
 import {lusitana} from '@/app/ui/fonts'
 import React, { useState } from 'react';
 export default function Page() {
